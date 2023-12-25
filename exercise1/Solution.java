c41a2f6891c396a5a3ee789cbc16b64ced8a3f9f
Han20nah <13963730799@163.com>
2023-12-25 20:36:43 +0800

    1. Valid Palindrome

---
+++ exercise1/Solution.java
@@ -0,0 +1,52 @@
+package exercise1;
+
+public class Solution {
+    /**
+     *  Topic:Valid Palindrome
+     * Determines whether the given string is a palindrome (reads the same forward and backward).
+     * Considers only alphanumeric characters and ignores other characters.
+     *
+     * @param s The given string
+     * @return true if it's a palindrome, false otherwise
+     */
+    public boolean isPalindrome(String s) {
+        // If the string is empty, it's considered a palindrome
+        if (s.isEmpty()) {
+            return true;
+        }
+
+        // Initialize start and last indices
+        int start = 0;
+        int last = s.length() - 1;
+
+        // Loop to check palindrome conditions
+        while (start <= last) {
+            char currFirst = s.charAt(start);
+            char currLast = s.charAt(last);
+
+            // If the starting character is not alphanumeric, move the start index forward
+            if (!Character.isLetterOrDigit(currFirst)) {
+                start++;
+            }
+            // If the ending character is not alphanumeric, move the last index backward
+            else if (!Character.isLetterOrDigit(currLast)) {
+                last--;
+            }
+            // If both starting and ending characters are alphanumeric
+            else {
+                // Compare lowercase characters for case-insensitive check
+                if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
+                    // If not equal, it's not a palindrome
+                    return false;
+                }
+                // Move start index forward and last index backward
+                start++;
+                last--;
+            }
+        }
+
+        // If the loop completes, it's a palindrome
+        return true;
+    }
+}
+
